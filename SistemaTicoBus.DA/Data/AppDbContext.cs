0dd808485047d62e859d78688ab44620327eedfb
Genesis Gutierrez <Gggensis2005@gmail.com>
2026-05-30 20:05:53 -0600

    Creacion de DA

---
+++ SistemaTicoBus.DA/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaTicoBus.MODEL.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaTicoBus.DA.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Ruta> Rutas { get; set; }
+    }
+}
