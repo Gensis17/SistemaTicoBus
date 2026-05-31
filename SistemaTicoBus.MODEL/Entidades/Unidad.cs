a636837e12601c690b2ae96f23654c56030cd18a
Genesis Gutierrez <Gggensis2005@gmail.com>
2026-05-30 21:38:29 -0600

    UNIDAD 5 MODEL

---
+++ SistemaTicoBus.MODEL/Entidades/Unidad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaTicoBus.MODEL.Entidades
+{
+    public class Unidad
+    {
+        public string Placa { get; set; }
+
+        public string Modelo { get; set; }
+
+        public int AnioFabricacion { get; set; }
+
+        public int CapacidadPasajeros { get; set; }
+    }
+}
