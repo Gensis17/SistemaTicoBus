6cb0ab1cd40e14ad52d9a4b899087e70e8fd4be6
Keity-Lr <keity.lopez@ucr.ac.cr>
2026-06-01 23:12:34 -0600

    Inicio de desarrollo del Módulo 9

---
+++ SistemaTicoBus.DA/Repositorios/ReservaRepositorio.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Data.SqlClient;
+using SistemaTicoBus.MODEL.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaTicoBus.DA.Repositorios
+{
+    public class ReservaRepositorio
+    {
+        private readonly string _connectionString = "Server=localhost\\SQLEXPRESS;Database=TicoBusDB;Trusted_Connection=True;TrustServerCertificate=True;";
+
+        public List<Reserva> ObtenerReservasPorPasajero(string nombreUsuario)
+        {
+            var lista = new List<Reserva>();
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                // JOIN para obtener el detalle de la reserva y el viaje
+                string query = @"SELECT r.IdReserva, r.NumeroAsiento, r.FechaReserva, 
+                                        v.FechaHoraSalida, ru.Nombre AS NombreRuta
+                                 FROM Reservas r
+                                 INNER JOIN Pasajeros p ON r.IdPasajero = p.Identificacion
+                                 INNER JOIN Usuarios u ON p.UsuarioId = u.Id
+                                 INNER JOIN Viajes v ON r.IdViaje = v.IdViaje
+                                 INNER JOIN Rutas ru ON v.IdRuta = ru.IdRuta
+                                 WHERE u.NombreUsuario = @NombreUsuario";
+
+                using (SqlCommand cmd = new SqlCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@NombreUsuario", nombreUsuario);
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            lista.Add(new Reserva
+                            {
+                                IdReserva = (int)reader["IdReserva"],
+                                NumeroAsiento = (int)reader["NumeroAsiento"],
+                                Viaje = new Viaje
+                                {
+                                    FechaHoraSalida = (DateTime)reader["FechaHoraSalida"],
+                                    Ruta = new Ruta { Nombre = reader["NombreRuta"].ToString() }
+                                }
+                            });
+                        }
+                    }
+                }
+            }
+            return lista;
+        }
+    }
+}
