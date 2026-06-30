0f09ee8e30a156c99ad5dd786342c3b1ff3c0a90
David <Davidsote280902@gmail.com>
2026-06-30 07:33:45 -0600

    Feat: Se agrega el proyecto .MAUI el cual contiene la aplicacion MAUI con su respectiva conexion a la API

---
+++ SistemaTicoBus.MAUI/AppShell.xaml.cs
@@ -0,0 +1,17 @@
+﻿using SistemaTicoBus.MAUI.Views;
+
+namespace SistemaTicoBus.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            Routing.RegisterRoute("InicioPage", typeof(InicioPage));
+            Routing.RegisterRoute("MisReservasPage", typeof(MisReservasPage));
+            Routing.RegisterRoute("DetalleReservaPage", typeof(DetalleReservaPage));
+            Routing.RegisterRoute("PerfilPage", typeof(PerfilPage));
+        }
+    }
+}
